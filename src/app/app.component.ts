282fd8eb60861b8c70d3b47b0831d07687d188d6
MSarfraz <mohammed.sarfraz@harman.com>
2020-04-10 15:30:38 +0530

    Covid-19 angular app 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+
+import {ViewEncapsulation, ViewChild, ElementRef, PipeTransform, Pipe, OnInit } from '@angular/core';
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +18,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covid-angular';
+
+  chartLinks: any =["https://docs.google.com/spreadsheets/d/e/2PACX-1vRdfmyZJaHWzwBA5RSmwKqyCKAHQgn2qVAeGaM2U3JUq1aEO-I5pAqINNRB3D4fe4e48T6GZxrYn-7w/pubchart?oid=256750270&amp;format=interactive",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdfmyZJaHWzwBA5RSmwKqyCKAHQgn2qVAeGaM2U3JUq1aEO-I5pAqINNRB3D4fe4e48T6GZxrYn-7w/pubchart?oid=547615245&amp;format=interactive",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdfmyZJaHWzwBA5RSmwKqyCKAHQgn2qVAeGaM2U3JUq1aEO-I5pAqINNRB3D4fe4e48T6GZxrYn-7w/pubchart?oid=1630971566&amp;format=interactive",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdfmyZJaHWzwBA5RSmwKqyCKAHQgn2qVAeGaM2U3JUq1aEO-I5pAqINNRB3D4fe4e48T6GZxrYn-7w/pubchart?oid=1042166760&amp;format=interactive",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdfmyZJaHWzwBA5RSmwKqyCKAHQgn2qVAeGaM2U3JUq1aEO-I5pAqINNRB3D4fe4e48T6GZxrYn-7w/pubchart?oid=1094328597&amp;format=interactive",
+                "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdfmyZJaHWzwBA5RSmwKqyCKAHQgn2qVAeGaM2U3JUq1aEO-I5pAqINNRB3D4fe4e48T6GZxrYn-7w/pubchart?oid=1950088878&amp;format=interactive"
+
+]
+
 }
